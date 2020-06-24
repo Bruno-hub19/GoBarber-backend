b509bf6ce9b2e5c5269dfbcc7a1dbc854a1716ec
Bruno <brunnofn19@gmail.com>
2020-06-24 12:53:45 -0300

    add ormconfig, typeorm and pg driver, first migration created

---
+++ src/database/migrations/1593013950734-CreateAppointments.ts
@@ -0,0 +1,7 @@
+import { MigrationInterface, QueryRunner } from 'typeorm';
+
+export class CreateAppointments1593013950734 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> { }
+
+  public async down(queryRunner: QueryRunner): Promise<void> { }
+}
