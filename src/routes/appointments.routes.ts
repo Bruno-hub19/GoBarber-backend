d83b3a57348af448891b6e61e3a81bf324dafb68
Bruno <brunnofn19@gmail.com>
2020-06-25 11:15:47 -0300

    change method to create a new appointment and return them

--- src/routes/appointments.routes.ts
+++ src/routes/appointments.routes.ts
@@ -1,38 +1,36 @@
 import { Router } from 'express';
+import { getCustomRepository } from 'typeorm';
 import { startOfHour, parseISO } from 'date-fns';
+
 import AppointmentsRepository from '../repositories/AppointmentsRepository';
+import CreateAppointmentService from '../services/CreateAppointmentService';
 
 const appointmentRoutes = Router();
 
-const appointmentsRepository = new AppointmentsRepository();
-
 appointmentRoutes.get('/', (request, response) => {
-  const allAppointments = appointmentsRepository.listAll();
+  const appointmentsRepository = getCustomRepository(AppointmentsRepository);
+  const appointments = appointmentsRepository.find();
 
-  return response.json(allAppointments);
+  return response.json(appointments);
 });
 
-appointmentRoutes.post('/', (request, response) => {
-  const { provider, date } = request.body;
+appointmentRoutes.post('/', async (request, response) => {
+  try {
+    const { provider, date } = request.body;
 
-  const parsedDate = startOfHour(parseISO(date));
+    const parsedDate = startOfHour(parseISO(date));
 
-  const findDuplicatedAppointment = appointmentsRepository.findByDate(
-    parsedDate,
-  );
+    const appointmentService = new CreateAppointmentService();
 
-  if (findDuplicatedAppointment) {
-    return response
-      .status(400)
-      .json({ error: 'This appointment is already booked' });
+    const appointment = await appointmentService.execute({
+      date: parsedDate,
+      provider,
+    });
+
+    return response.json(appointment);
+  } catch (err) {
+    return response.status(400).json({ error: err.message });
   }
-
-  const appointment = appointmentsRepository.create({
-    provider,
-    date: parsedDate,
-  });
-
-  return response.json(appointment);
 });
 
 export default appointmentRoutes;
