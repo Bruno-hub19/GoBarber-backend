187e37e96cbf8d3939e6ee1e4b9b8191d4e90402
Bruno <brunnofn19@gmail.com>
2020-06-26 16:45:46 -0300

    add session routes, authenticated service, hashing pass and dotenv

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,10 +2,12 @@ import { Router } from 'express';
 
 import appointmentsRoutes from './appointments.routes';
 import usersRoutes from './users.routes';
+import sessionsRoutes from './sessions.routes';
 
 const routes = Router();
 
 routes.use('/appointment', appointmentsRoutes);
 routes.use('/user', usersRoutes);
+routes.use('/session', sessionsRoutes);
 
 export default routes;
