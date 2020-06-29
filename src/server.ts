a76940474dcd868162807ff6009cce5a44eed24b
Bruno <brunnofn19@gmail.com>
2020-06-29 09:10:02 -0300

    add eslint console rule, add app file

--- src/server.ts
+++ src/server.ts
@@ -1,37 +1,5 @@
-import 'reflect-metadata';
-
-import express, { Request, Response, NextFunction } from 'express';
-import 'express-async-errors';
-
-import './database';
-import routes from './routes/index';
-import uploadConfigs from './configs/upload';
-import AppError from './errors/AppError';
-
-const app = express();
-
-app.use(express.json());
-app.use('/files', express.static(uploadConfigs.directory));
-app.use(routes);
-
-app.use((err: Error, request: Request, response: Response, _: NextFunction) => {
-  if (err instanceof AppError) {
-    return response.status(err.statusCode).json({
-      status: 'error',
-      message: err.message,
-    });
-  }
-
-  // eslint-disable-next-line no-console
-  console.error(err);
-
-  return response.status(500).json({
-    status: 'error',
-    message: 'Internal server error',
-  });
-});
+import app from './app';
 
 app.listen(3333, () => {
-  // eslint-disable-next-line no-console
   console.log('Server running => http://localhost:3333');
 });
