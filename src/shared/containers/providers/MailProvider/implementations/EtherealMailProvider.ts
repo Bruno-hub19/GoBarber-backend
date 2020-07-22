c321dfbe115145abcc47cdb65bd3720821cc9516
Bruno <brunnofn19@gmail.com>
2020-07-22 13:58:22 -0300

    add forgot password mail template

--- src/shared/containers/providers/MailProvider/implementations/EtherealMailProvider.ts
+++ src/shared/containers/providers/MailProvider/implementations/EtherealMailProvider.ts
@@ -1,16 +1,23 @@
 import nodemailer, { Transporter } from 'nodemailer';
 
+import IMailTemplateProvider from '@shared/containers/providers/MailTemplateProvider/models/IMailTemplateProvider';
+import { inject, injectable } from 'tsyringe';
 import IMailProvider from '../models/IMailProvider';
+import ISendMailDTO from '../dtos/ISendMailDTO';
 
 interface IMessage {
   to: string;
   body: string;
 }
 
+@injectable()
 class EtherealMailProvider implements IMailProvider {
   private client: Transporter;
 
-  constructor() {
+  constructor(
+    @inject('MailTemplateProvider')
+    private mailTemplateProvider: IMailTemplateProvider,
+  ) {
     nodemailer.createTestAccount().then(account => {
       const transporter = nodemailer.createTransport({
         host: account.smtp.host,
@@ -26,12 +33,20 @@ class EtherealMailProvider implements IMailProvider {
     });
   }
 
-  public async sendMail(to: string, body: string): Promise<void> {
+  public async sendMail({
+    from,
+    to,
+    subject,
+    templateData,
+  }: ISendMailDTO): Promise<void> {
     const message = await this.client.sendMail({
-      from: 'Equipe GoBarber <equipegobarber@mail.com>',
-      to,
-      subject: 'Recuperação de senha',
-      text: body,
+      from: {
+        name: from?.name || 'Equipe GoBarber',
+        address: from?.email || 'equipe@gobarber.com.br',
+      },
+      to: { name: to.name, address: to.email },
+      subject,
+      html: await this.mailTemplateProvider.parse(templateData),
     });
 
     console.log('Message sent: %s', message.messageId);
