bd24047845cae29ecdc024c29bb81968be2944a8
Bruno <brunnofn19@gmail.com>
2020-08-15 06:20:02 -0300

    add query params and class transformer

--- src/modules/appointments/infra/http/controllers/ProviderAppointmentsController.ts
+++ src/modules/appointments/infra/http/controllers/ProviderAppointmentsController.ts
@@ -1,12 +1,13 @@
 import { Request, Response } from 'express';
 import { container } from 'tsyringe';
+import { classToClass } from 'class-transformer';
 
 import ListProviderAppointmentsService from '@modules/appointments/services/ListProviderAppointmentsService';
 
 class ProviderAppointmentsController {
   public async index(request: Request, response: Response): Promise<Response> {
     const provider_id = request.user.id;
-    const { year, month, day } = request.body;
+    const { year, month, day } = request.query;
 
     const listProviderAppointmentsService = container.resolve(
       ListProviderAppointmentsService,
@@ -14,12 +15,12 @@ class ProviderAppointmentsController {
 
     const appointments = await listProviderAppointmentsService.execute({
       provider_id,
-      year,
-      month,
-      day,
+      year: Number(year),
+      month: Number(month),
+      day: Number(day),
     });
 
-    return response.json(appointments);
+    return response.json(classToClass(appointments));
   }
 }
 
--- src/modules/appointments/infra/http/controllers/ProviderMonthAvailabilityController.ts
+++ src/modules/appointments/infra/http/controllers/ProviderMonthAvailabilityController.ts
@@ -6,7 +6,7 @@ import ListProviderMonthAvailabilityService from '@modules/appointments/services
 class ProviderMonthAvailabilityController {
   public async index(request: Request, response: Response): Promise<Response> {
     const { provider_id } = request.params;
-    const { year, month } = request.body;
+    const { year, month } = request.query;
 
     const listProviderMonthAvailabilityService = container.resolve(
       ListProviderMonthAvailabilityService,
@@ -15,8 +15,8 @@ class ProviderMonthAvailabilityController {
     const monthAvailability = await listProviderMonthAvailabilityService.execute(
       {
         provider_id,
-        year,
-        month,
+        year: Number(year),
+        month: Number(month),
       },
     );
 
