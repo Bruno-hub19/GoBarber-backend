9fc89e4638fdb77b0e2d60b07093e119446a8e2d
Bruno <brunnofn19@gmail.com>
2020-08-15 06:23:41 -0300

    pass provider id as argument into find by date method

--- src/modules/appointments/services/CreateAppointmentService.ts
+++ src/modules/appointments/services/CreateAppointmentService.ts
@@ -50,6 +50,7 @@ class CreateAppointmentService {
 
     const appointmentAlreadyBooked = await this.appointmentsRepository.findByDate(
       appointmentDate,
+      provider_id,
     );
 
     if (appointmentAlreadyBooked) {
